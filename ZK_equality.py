67e75346769af72aac8cd7b7bef9b6edd4e80d2c
AmberChaoYang <chaoyang@seas.upenn.edu>
2021-04-03 20:59:46 -0400

    Update ZK_equality.py

--- ZK_equality.py
+++ ZK_equality.py
@@ -14,35 +14,52 @@ from zksk import Secret, DLRep
 from zksk import utils
 
 
-# In[3]:
+# In[2]:
 
 
 def ZK_equality(G,H):
 
 ##Generate two El-Gamal ciphertexts (C1,C2) and (D1,D2)
     # Setup: generate a secret randomizer.
+    
+    # M is the secret bit.
+    #m = 1
+    m = Secret(utils.get_random_num(bits=128))
     r1 = Secret(utils.get_random_num(bits=128))
     r2 = Secret(utils.get_random_num(bits=128))
-    C1 = r1.value * G
-    C2 = r2.value * G
+    #print("this is r1 and r2:", r1.value,",",r2.value)
+    C1 = r1* G
+    #C2 = m * G + r1.value * H
+    D1 = r2 * G
+    #D2 = m * G + r2.value * H
+    #C1 = r1 * G
+    C2 = m * G + r1* H
+    #D1 = r2 * G
+    D2 = m * G + r2* H    
   
-    
-    # This is Peggy's secret bit.
-    top_secret_bit = 1
-    
 
-    # A Pedersen commitment to the secret bit.
-    D1 = top_secret_bit * G + r1.value * H
-    D2 = top_secret_bit * G + r2.value * H
+    # A Pedersen commitment to the secret bit.   
     
-    
-##Generate a NIZK proving equality of the plaintexts     
-    stmt = DLRep(C1,r1*G) & DLRep(C2,r1*H+m*G) & DLRep(D1,r2*G) & DLRep(D2,r2*H+m*G)
+##Generate a NIZK proving equality of the plaintexts 
+    #print("this is DLRep(C1,r1*G):", DLRep(C1,r1*G))
+    stmt = DLRep(C1,r1*G) & DLRep(C2,m*G+r1*H) & DLRep(D1,r2*G) & DLRep(D2,m*G+r2*H)
     zk_proof = stmt.prove()
     
     
     #Return two ciphertexts and the proof
     return (C1,C2), (D1,D2), zk_proof
+
+
+# In[3]:
+
+
+G, H = utils.make_generators(num=2, seed=42)
+
+
+# In[4]:
+
+
+ZK_equality(G,H)
 
 
 # In[ ]:
